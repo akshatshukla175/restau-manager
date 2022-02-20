44461ccce1f7be1f74aefbd9efe6d5b572034731
Akshat Shukla <akshatshukla175@gmail.com>
2022-02-20 17:39:26 +0530

    adding project files and establishing routes

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"gin-poc/database"
+	"gin-poc/middleware"
+	"gin-poc/routes"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"os"
+)
+
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+	router := gin.New()
+	router.Use(gin.Logger())
+	routes.UserRoutes(router)
+	router.Use(middleware.Authentication())
+
+	routes.FoodRoutes(router)
+	routes.MenuRoutes(router)
+	routes.TableRoutes(router)
+	routes.OrderRoutes(router)
+	routes.OrderItemRoutes(router)
+	routes.InvoiceRoutes(router)
+
+	router.Run(":" + port)
+}
